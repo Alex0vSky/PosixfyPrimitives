34a1e38a1ff6dd2b7fa3170f5c3e943a45f85e2d
Alex0vSky <52796897+Alex0vSky@users.noreply.github.com>
2024-03-07 20:44:18 +0300

    Publishing branch

---
+++ PosixfyPrimitives.cpp
@@ -0,0 +1,20 @@
+﻿// PosixfyPrimitives.cpp - entry point // Copyright 2024 Alex0vSky (https://github.com/Alex0vSky)
+
+int main(int argc, char **argv) {
+#if ( defined( _DEBUG ) ) & ( defined( _WIN32 ) )
+    HeapSetInformation( NULL, HeapEnableTerminationOnCorruption, NULL, NULL );
+	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+	//_CrtSetBreakAlloc( 161 );
+#	ifdef new
+#		error `new` has been redefined
+#	endif
+	new char[]{ "Goobay!" };
+#endif // ( defined( _DEBUG ) ) & ( defined( _WIN32 ) )
+
+	// for `boost::system::error_code::message` language
+	setlocale( 0, "" );
+
+	::testing::InitGoogleTest( &argc, argv );
+	std::cout << "Running main() from Posixer.cpp" << std::endl;
+	return RUN_ALL_TESTS( );
+}
---
+++ _msvcStuff/stdafx.h
@@ -0,0 +1,30 @@
+﻿// stdafx.h - pch
+
+#ifdef __clang__
+#	pragma clang diagnostic push
+#	pragma clang diagnostic ignored "-Wmicrosoft-include"
+#endif // __clang__
+
+#include "include/gtest/gtest.h"
+#include "include/gmock/gmock.h"
+
+#ifdef __clang__
+#	pragma clang diagnostic pop
+#endif // __clang__
+
+#pragma warning( push )
+//#pragma warning( disable: 5039 )
+
+// System
+#define NOMINMAX
+#ifdef _DEBUG
+#	include <crtdbg.h>
+#endif
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <intrin.h>
+#include <stdlib.h>
+
+//// boost
+//#include <boost/Xxx.hpp>
+#pragma warning( pop )
