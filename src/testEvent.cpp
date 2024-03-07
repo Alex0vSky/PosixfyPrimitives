f495391696e71d6d2cc73dc946af4e1b2b6e67f7
Alex0vSky <52796897+Alex0vSky@users.noreply.github.com>
2024-03-07 23:29:52 +0300

    cache and CMAKE_INSTALL_PREFIX

--- src/testEvent.cpp
+++
@@ -1,9 +0,0 @@
-﻿// src/testEvent.cpp - event facility // Copyright 2024 Alex0vSky (https://github.com/Alex0vSky)
-
-TEST(event, truerly) { 
-	EXPECT_TRUE( true );
-}
-
-TEST(event, failure) { 
-	EXPECT_TRUE( false );
-}
---
+++ tests/testEvent.cpp
@@ -0,0 +1,9 @@
+﻿// tests/testEvent.cpp - test posixfy event // Copyright 2024 Alex0vSky (https://github.com/Alex0vSky)
+
+TEST(event, truerly) { 
+	EXPECT_TRUE( true );
+}
+
+TEST(event, failure) { 
+	EXPECT_TRUE( false );
+}
