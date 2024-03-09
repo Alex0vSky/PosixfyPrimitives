3f1147ce4ed9e175bcd0687840006d3e5001aaa0
Alex0vSky <52796897+Alex0vSky@users.noreply.github.com>
2024-03-10 00:00:10 +0300

    recheck

--- tests/testSharedMem.cpp
+++ tests/testSharedMem.cpp
@@ -8,7 +8,8 @@
 using CSharedMem = Ipc::CSharedMem;
 static const unsigned c_size = 4096;
 static char g_name[] = "my_lucky_unique_name";
-/*
+
+//*
 namespace testSharedMemory_ { 
 
 TEST(SharedMemory_create, already_exists) {
